829f537fa00260378bb8ade8464b0d9fb09b53d9
rkugot <stream13k@gmail.com>
2017-07-22 11:10:34 +0300

    Add categories functionality

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,10 +10,12 @@ class BooksController < ApplicationController
 
   def new
     @book = current_user.books.build
+    @categories = Category.all.map{ |c| [c.name, c.id] }
   end
 
   def create
     @book = current_user.books.build(book_params)
+    @book.category_id = params[:category_id]
     if @book.save
       redirect_to root_path
     else
@@ -22,9 +24,11 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @categories = Category.all.map{ |c| [c.name, c.id] }
   end
 
   def update
+    @book.category_id = params[:category_id]
     if @book.update(book_params)
       redirect_to book_path(@book)
     else
@@ -33,14 +37,12 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    @book.destroy
-    redirect_to root_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title, :description, :author)
+    params.require(:book).permit(:title, :description, :author, :category_id)
   end
 
   def find_book
